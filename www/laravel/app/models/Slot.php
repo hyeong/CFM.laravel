2a3159472b8dfef9c7fd954e9d45f81a513ce18d
Jon Spriggs <jon@sprig.gs>
2014-08-23 23:50:34 +0100

    Set the default value for the slottype_id value.

--- www/laravel/app/models/Slot.php
+++ www/laravel/app/models/Slot.php
@@ -7,4 +7,20 @@ class Slot extends GenericModel {
 		'start' => 'required',
 		'end' => 'required',
 	];
+
+	// As found http://stackoverflow.com/a/20621344/5738
+	// Note, if your construct function needs to change
+	// the attributes, use 
+	// public function __construct(array $attributes = array())
+	// {
+	// $this->setRawAttributes(
+	//	array_merge(
+	//		$this->attributes,
+	//		['my' => 'thing']
+	//	), true);
+	// parent::construct($attributes);
+	// }
+	protected $attributes = array (
+		'slottype_id' => 0
+	);
 }
